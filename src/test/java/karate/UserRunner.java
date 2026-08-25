67f16a55b2067505184d4ec4fd212a2f845abddf
Christian Delgado <christianeds0908@gmail.com>
2026-08-25 16:48:43 -0500

    cucumber.json

--- src/test/java/karate/UserRunner.java
+++ src/test/java/karate/UserRunner.java
@@ -5,8 +5,8 @@ import com.intuit.karate.junit5.Karate;
 public class UserRunner {
 
     @Karate.Test
-    Karate testTodo(){
+    Karate testTodo() {
 
-        return Karate.run("classpath:functional/features/user");
+        return Karate.run("classpath:functional/features/user").outputCucumberJson(true);
     }
 }
